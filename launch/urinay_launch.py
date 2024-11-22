31319e9fcae994acc97e38fd423ca4851ebba939
Filippo Gibertini <filippogib@gmail.com>
2024-11-22 09:14:33 +0100

    fixed launch file

---
+++ launch/urinay_launch.py
@@ -0,0 +1,25 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    config_node = os.path.join(
+        get_package_share_directory('urinay'),
+        'config',
+        'urinay.yaml'
+        )
+
+    node=Node(
+            package='urinay',
+            # namespace='clustering_ground_segmentation',
+            name='urinay',
+            executable='urinay',
+            parameters=[config_node]
+        )
+
+    ld.add_action(node)
+    return ld
